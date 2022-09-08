d6383b728d95e8b07c9dec26d6e266a7f2852913
mvincentbb <mvincentbb@gmail.com>
2022-09-08 17:37:33 +0100

    exo 3

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main -  print "hello World"
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
