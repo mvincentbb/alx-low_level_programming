af3456318590634de3cae2a52a077fff2ff09853
mvincentbb <mvincentbb@gmail.com>
2022-09-13 23:37:22 +0100

    exo 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _abs - print the absolute
+* @n: The number to return it absolute
+*
+* Return: the absolute number
+*/
+int _abs(int n)
+{
+	int abs;
+
+	if (n >= 0)
+	{
+		return (int);
+	}
+	else
+	{
+		abs = n * -1;
+		return (abs);
+	}
+}
