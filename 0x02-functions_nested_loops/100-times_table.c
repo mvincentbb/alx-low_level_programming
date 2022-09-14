414eb548e0b663580b749b7b00ce33fc58f7f4b3
mvincentbb <mvincentbb@gmail.com>
2022-09-14 21:56:36 +0100

    exo 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,41 +9,42 @@ void print_times_table(int n)
 	int i, j, res, d, u, c;
 
 	if (n > 15 || n < 0)
+		_putchar(' ');
 	else
 	{
 		for (i = 0; i <= n; i++)
 		{
-			putchar('0');
+			_putchar('0');
 			for (j = 1; j <= n; j++)
 			{
-				putchar(',');
-				putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 				res = i * j;
 				if (res <= 9)
 				{
-					putchar(' ');
-					putchar(' ');
-					putchar('0' + res);
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + res);
 				}
 				else if (res <= 99)
 				{
 					u = res % 10;
 					d = res / 10;
-					putchar(' ');
-					putchar('0' + d);
-					putchar('0' + u);
+					_putchar(' ');
+					_putchar('0' + d);
+					_putchar('0' + u);
 				}
 				else
 				{
 					u = res % 10;
 					d = (res / 10) - 10;
 					c = res / 100;
-					putchar('0' + c);
-					putchar('0' + d);
-					putchar('0' + u);
+					_putchar('0' + c);
+					_putchar('0' + d);
+					_putchar('0' + u);
 				}
 				if (j == n)
-					putchar('\n');
+					_putchar('\n');
 			}
 		}
 	}
