d92e2e1bd7db85e15742534aa60b834a87634e0b
mvincentbb <mvincentbb@gmail.com>
2022-09-15 00:21:56 +0100

    exo 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* natural_numbers - mutiple of 3 or 5
+*
+* @n: integer
+*
+* Return: Always 0
+*/
+void natural_number(int n)
+{
+	int i, sum;
+
+	for (i = 0; i < n; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d", sum);
+}
