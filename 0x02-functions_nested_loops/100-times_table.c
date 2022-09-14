c048500d4781e801f5855fd4d0aee997746e5ebd
mvincentbb <mvincentbb@gmail.com>
2022-09-14 22:29:32 +0100

    exo 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,10 +31,9 @@ void print_times_table(int n)
 				{
 					_putchar((res / 10) + '0');
 				}
-				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
 			}
-			_putchar((res % 10) + '0');
+			_putchar('\n');	
 		}
-		_putchar('\n');
 	}
 }
