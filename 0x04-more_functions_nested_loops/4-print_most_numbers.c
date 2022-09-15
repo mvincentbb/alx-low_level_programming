f0d1d666e030f975449612bc93265ed4efa6b184
mvincentbb <mvincentbb@gmail.com>
2022-09-16 00:38:33 +0100

    exo 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_most_numbers - print numbers
+*
+* Return: 0
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ( i != 2 && i != 4)
+			_putchar('0' + i);
+	}
+	_putchar('\n');
+}
