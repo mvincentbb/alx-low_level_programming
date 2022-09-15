c5e5258879c7598f1fdb1afea9c5d8f36ecf8a86
mvincentbb <mvincentbb@gmail.com>
2022-09-16 00:53:25 +0100

    exo 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* more_numbers - more
+*
+* Return: 0
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar('0' + j);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
