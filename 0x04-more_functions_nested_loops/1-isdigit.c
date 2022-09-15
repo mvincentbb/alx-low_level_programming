5f78db4c1ac8ddbd50e685fb6e3a92dd179873f4
mvincentbb <mvincentbb@gmail.com>
2022-09-16 00:06:52 +0100

    exo 0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isdigit - isdigit
+*
+* @c: integer
+*
+* Return: 1 if digit otherwise 0
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 $$ c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
