d19257b02e75c517e7da69e4d84101084ee5dca0
BAKPATINA Vincent <47268786+mvincentbb@users.noreply.github.com>
2022-09-20 01:54:22 +0000

    exo 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps int a and int b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
