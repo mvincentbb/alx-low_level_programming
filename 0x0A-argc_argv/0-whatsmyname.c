ed721c6582f3b238d863aa0df8cf5764b568a116
mvincentbb <mvincentbb@gmail.com>
2022-09-30 23:11:37 +0100

    exo 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
 * main - program that prints its name, followed by a new line.
@@ -8,5 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-	
+	if (argc)
+		printf("%s\n", argv[0]);
+	return (0);
 }
