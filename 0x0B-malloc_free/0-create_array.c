d9c9e12e3d32790672668d93408444b65989023a
mvincentbb <mvincentbb@gmail.com>
2022-10-05 00:32:39 +0100

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
-	
+
 	if (size == 0)
-		return (0);
+		return (NULL);
 	arr = malloc(size * sizeof(char));
-	arr[0] = c;
-	return (arr);
+	if (arr != NULL)
+	{
+		unsigned int i;
+
+		for (i = 0; i < size; i++)
+		{
+			arr[i] = c;
+		}
+		return (arr);
+	}
+	return (NULL);
 }
