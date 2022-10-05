8d31d16768424d96e80a6b3cf11f5795eb4c428d
mvincentbb <mvincentbb@gmail.com>
2022-10-05 01:17:59 +0100

    exo 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: The string to get the length
+ *
+ * Return: length of @s.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	i++;
+	return (i);
+}
+
+/**
+* str_concat - function that concatenates two strings
+*
+* @s1: char
+* @s2: char
+*
+* Return: returns a concatenates string from s1 and s2
+*/
+char *str_concat(char *s1, char *s2)
+{
+	int lenT, len1, len2, i, j;
+	char *str;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		str = malloc(sizeof(char));
+		str[0] = '\0';
+		return (str);
+	}
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	lenT = len1 + len2;
+	str = malloc(sizeof(char) * lenT);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < len1 ; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; j < len2; j++)
+	{
+		str[i + j] = s2[j];
+	}
+	return (str);
+}
