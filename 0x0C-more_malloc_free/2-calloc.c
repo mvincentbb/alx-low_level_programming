c44eb8db534e3920a80c36ee703b5c1841491803
mvincentbb <mvincentbb@gmail.com>
2022-10-07 01:41:30 +0100

    exo 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	n = nmemb * size + 1;
+	n = nmemb * size;
 	p = malloc(n);
 	if (p == NULL)
 		return (NULL);
