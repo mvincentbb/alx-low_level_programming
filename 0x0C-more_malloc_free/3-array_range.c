7788eb065fd6e879f4ff63848fa318688553d785
mvincentbb <mvincentbb@gmail.com>
2022-10-07 01:31:28 +0100

    exo 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	n = max - min + 2;
+	n = max - min + 1;
 	arr = malloc(sizeof(int) * n);
 	if (arr == NULL)
 		return (NULL);
