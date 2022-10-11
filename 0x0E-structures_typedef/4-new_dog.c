9e9bec63e28c263eb7e355b96001e142c16708ec
mvincentbb <mvincentbb@gmail.com>
2022-10-11 03:49:12 +0100

    exo 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,1 +1,25 @@
+#include <stdlib.h>
+#include "dog.h"
 
+/**
+* new_dog - new doc
+*
+* @name: name
+* @age: age
+* @owner: owner
+*
+* Return: a new dog
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
+}
